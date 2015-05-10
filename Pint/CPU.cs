d6b0e4aeede3f3aabe0d4908353359f6480e359d
Mirinth <mirinth@gmail.com>
2015-05-10 12:53:10 -0600

    Added CPU constructor

--- Pint/CPU.cs
+++ Pint/CPU.cs
@@ -7,6 +7,23 @@ namespace Pint
     /// </summary>
     public static class CPU
     {
+        private const int register_count = 32;
 
+        private static UInt32 _ip;
+        private static UInt32[] _registers;
+
+        /// <summary>
+        /// Initializes the CPU
+        /// </summary>
+        static CPU()
+        {
+            _ip = 0;
+            _registers = new UInt32[register_count];
+
+            for (int i = 0; i < register_count; i++)
+            {
+                _registers[i] = 0;
+            }
+        }
     }
 }
