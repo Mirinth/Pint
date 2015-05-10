07f6e14c4f50617b1ed007e64a5c355270275375
Mirinth <mirinth@gmail.com>
2015-05-10 12:03:26 -0600

    Added MemoryManager

---
+++ Pint/MemoryManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Pint
+{
+    /// <summary>
+    /// Manages the memory visible to the program.
+    /// </summary>
+    class MemoryManager
+    {
+        private byte[] _code;
+        private byte[] _data;
+
+        /// <summary>
+        /// Initializes the memory manager some blank data memory
+        /// and no code memory.
+        /// </summary>
+        public MemoryManager()
+        {
+            const int MAX_DATA_MEMORY = 0xffff;
+            _code = new byte[0];
+            _data = new byte[MAX_DATA_MEMORY];
+        }
+    }
+}
