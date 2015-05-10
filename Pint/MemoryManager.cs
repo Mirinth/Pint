02b6e2d54ead0905e6d49b59fd853c4ace8b45d8
Mirinth <mirinth@gmail.com>
2015-05-10 12:07:21 -0600

    Added LoadCode method

--- Pint/MemoryManager.cs
+++ Pint/MemoryManager.cs
@@ -17,8 +17,23 @@ namespace Pint
         public MemoryManager()
         {
             const int MAX_DATA_MEMORY = 0xffff;
-            _code = new byte[0];
+            _code = null;
             _data = new byte[MAX_DATA_MEMORY];
+        }
+
+        /// <summary>
+        /// Initializes the program's code space.
+        /// Code is read-only and may only be initialized once.
+        /// </summary>
+        /// <param name="codeImage">An image of the program's code.</param>
+        {
+            if (_code != null)
+            {
+                throw new InvalidOperationException("Re-initialization of the memory manager is unsupported");
+            }
+
+            _code = new byte[codeImage.Length];
+            codeImage.CopyTo(_code, 0);
         }
     }
 }
