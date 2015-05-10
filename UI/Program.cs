23232f213a8bca60713ac5545a7fb651d82f48f2
Mirinth <mirinth@gmail.com>
2015-05-10 12:42:01 -0600

    Added UI project to solution

---
+++ UI/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+
+namespace UI
+{
+    /// <summary>
+    /// Contains the main entry point for the program.
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// The main entry point for the program.
+        /// </summary>
+        /// <param name="args">Ignored.</param>
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
