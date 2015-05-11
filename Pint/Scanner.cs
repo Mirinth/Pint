744d2869a062bfecc5a4ddc83734138ce1d68e76
Mirinth <mirinth@gmail.com>
2015-05-11 15:12:25 -0600

    Added Scanner.Scan() method

--- Pint/Scanner.cs
+++ Pint/Scanner.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.IO;
 
 namespace Pint
 {
@@ -8,6 +8,41 @@ namespace Pint
     /// </summary>
     static class Scanner
     {
+        /// <summary>
+        /// Reads the contents of a TextReader and breaks them up into pieces.
+        /// </summary>
+        /// <param name="reader">The data source.</param>
+        /// <returns>The contents of the data source, broken into pieces.</returns>
+        public static List<string> Scan(TextReader reader)
+        {
+            const int eof = -1;
+
+            List<string> pieces = new List<string>();
+            string token = string.Empty;
+
+            while (reader.Peek() != eof)
+            {
+                if (token.Length == 0 ||
+                    IsWhitespace(token[token.Length - 1]) == IsWhitespace((char)reader.Peek()))
+                {
+                    token += (char)reader.Read();
+                }
+                else
+                {
+                    pieces.Add(token);
+                    token = string.Empty;
+                    token += (char)reader.Read();
+                }
+            }
+
+            if (token.Length > 0)
+            {
+                pieces.Add(token);
+            }
+
+            return pieces;
+        }
+
         /// <summary>
         /// Determines whether a character is whitespace.
         /// </summary>
