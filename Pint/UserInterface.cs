e03c6b07f9b70957d8910c41d239f4286b157ee3
Mirinth <mirinth@gmail.com>
2015-05-10 21:48:34 -0600

    Added UserInterface.cs

---
+++ Pint/UserInterface.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Pint
+{
+    /// <summary>
+    /// This is the stuff that everybody else who uses the library can see.
+    /// </summary>
+    public class UserInterface
+    {
+        /// <summary>
+        /// Initializes the UserInterface.
+        /// </summary>
+        public UserInterface() { } // Actually, there's nothing to do here.
+    }
+}
