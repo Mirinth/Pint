306432dca8002c8ebb71e99d85dc6891d4a2568f
Mirinth <mirinth@gmail.com>
2015-05-11 14:52:57 -0600

    Added Scanner.cs

---
+++ Pint/Scanner.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Pint
+{
+    /// <summary>
+    /// The scanner takes in a code file and breaks it into pieces.
+    /// </summary>
+    static class Scanner
+    {
+    }
+}
