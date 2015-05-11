f279354eddaa5e1a8b9955c595e7f609f3669d21
Mirinth <mirinth@gmail.com>
2015-05-11 15:18:54 -0600

    Added Parser.Parse() method

--- Pint/Parser.cs
+++ Pint/Parser.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace Pint
 {
-    class Parser
+    /// <summary>
+    /// The parser takes a list of pieces collected by the scanner,
+    /// validates them and returns a runnable code.
+    /// </summary>
+    public static class Parser
     {
+        /// <summary>
+        /// Takes a list of pieces collected by the scanner, validates
+        /// them and returns a runnable code.
+        /// </summary>
+        /// <param name="pieces">A list of pieces collected by the scanner.</param>
+        /// <returns>A runnable code.</returns>
+        public static Dictionary<string, UInt32> Parse(List<string> pieces)
+        {
+
+        }
     }
 }
