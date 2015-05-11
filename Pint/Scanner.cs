88f57cb61733be7efbfbc5e42610e5bc10405600
Mirinth <mirinth@gmail.com>
2015-05-11 15:06:34 -0600

    Added IsWhitespace() to Scanner

--- Pint/Scanner.cs
+++ Pint/Scanner.cs
@@ -8,5 +8,23 @@ namespace Pint
     /// </summary>
     static class Scanner
     {
+        /// <summary>
+        /// Determines whether a character is whitespace.
+        /// </summary>
+        /// <param name="c">The character to test.</param>
+        /// <returns>Whether the character is a whitespace character.</returns>
+        public static bool IsWhitespace(char c)
+        {
+            // Pint doesn't care about whitespace other than \r, \n, and space
+            // right now.
+            if (c == '\r' || c == '\n' || c == ' ')
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
